cdd656cb6c7485933dd379d2258309449d20c041
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-10 18:52:54 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -27,7 +27,7 @@ def execute_transaction
 end
 
 def reverse_transfer
-  if self.status == "complete" && receiver.balance >=amount
+  if self.status == "complete"
     receiver.balance -= amount
     sender.balance += amount
   end
