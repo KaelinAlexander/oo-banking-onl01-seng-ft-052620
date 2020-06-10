4d193d4945af2fb3c726e1b780d062314a59035c
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-10 18:24:56 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -19,7 +19,7 @@ class BankAccount
   def valid?
     if self.balance > 0 && self.status = "open"
     return true
-    else
+    elsif self.balance >= 0 || self.status = "closed"
     return false
     end
   end
