909d544e04b2e6bf8f0f1ec8280c68a4ced66596
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-10 18:57:57 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ def valid?
 end
 
 def execute_transaction
-  if status == "pending" && sender.balance >= amount
+  if status == "pending" && sender.balance >= amount && sender.
     sender.balance -= amount
     receiver.balance += amount
     self.status = "complete"
