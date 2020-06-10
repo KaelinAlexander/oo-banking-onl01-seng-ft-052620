43847f50926a75fda36edd87e40d1f568a1430c2
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-10 18:30:59 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,5 +1,12 @@
 class Transfer
+attr_accessor :status
+attr_reader :sender, :receiver, :amount
 
-
+def initialize(sender, receiver, amount)
+  @sender = sender
+  @receiver = receiver
+  @amount = amount
+  @status = "pending"
+end
 
 end
